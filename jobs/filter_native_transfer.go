594f4973dc23da78920ed5cb7921a69200037a09
Nya Candy <dev@candinya.com>
2024-01-14 20:44:53 +0800

    fix: skip logic

--- jobs/filter_native_transfer.go
+++ jobs/filter_native_transfer.go
@@ -14,7 +14,7 @@ import (
 
 func filterNativeTransfer(client *ethclient.Client, fromBlock uint64, toBlock uint64) (filteredLogs []types.FilterParsedLog, err error) {
 
-	if fromBlock < toBlock {
+	if fromBlock > toBlock {
 		// Just nothing
 		return nil, nil
 	}
